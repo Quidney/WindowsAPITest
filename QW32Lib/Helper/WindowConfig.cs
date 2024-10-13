cda3568095d96196dde25203f60639b64cd61d78
Quidney <quidney0@gmail.com>
2024-10-13 11:02:00 +0200

    Add api call ebctraction via WindowConfig

---
+++ QW32Lib/Helper/WindowConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace QW32Lib.Helper
+{
+    public struct WindowConfig(
+        uint _dwExStyle, 
+        string _lpClassName,
+        string _lpWindowName,
+        uint _dwStyle,
+        int _X,
+        int _Y,
+        int _nWidth,
+        int _nHeight,
+        IntPtr _hWndParent,
+        IntPtr _hMenu,
+        IntPtr _hInstance,
+        IntPtr _lpParam
+        )
+    {
+        public uint dwExStyle = _dwExStyle;
+        public string lpClassName = _lpClassName;
+        public string lpWindowName = _lpWindowName;
+        public uint dwStyle = _dwStyle;
+        public int X = _X;
+        public int Y = _Y;
+        public int nWidth = _nWidth;
+        public int nHeight = _nHeight;
+        public IntPtr hWndParent = _hWndParent;
+        public IntPtr hMenu = _hMenu;
+        public IntPtr hInstance = _hInstance;
+        public IntPtr lpParam = _lpParam;
+    }
+}
