a19449d3f6cf1135513e0bfb2be5befe4c62a01a
Quidney <quidney0@gmail.com>
2024-10-15 12:32:17 +0200

    Change inline decleration to move MSG to outside of the loop scope for performance.

--- QW32Lib/Application.cs
+++ QW32Lib/Application.cs
@@ -11,7 +11,8 @@ namespace QW32Lib
 
         private static void MessageLoop()
         {
-            while (User32.GetMessageW(out MSG msg, IntPtr.Zero, 0, 0))
+            MSG msg;
+            while (User32.GetMessageW(out msg, IntPtr.Zero, 0, 0))
             {
                 User32.TranslateMessage(ref msg);
                 User32.DispatchMessageW(ref msg);
