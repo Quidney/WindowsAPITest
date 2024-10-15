ee350869ccec7f2b8efad7ce99dbf2395681cb45
Quidney <quidney0@gmail.com>
2024-10-15 11:01:13 +0200

    Create application class

---
+++ QW32Lib/Application.cs
@@ -0,0 +1,22 @@
+﻿using QW32Lib.DataTypes;
+
+namespace QW32Lib
+{
+    /// <summary>
+    /// Abstraction
+    /// </summary>
+    public static class Application
+    {
+        public static void Run() => MessageLoop();
+
+        private static void MessageLoop()
+        {
+            while (User32.GetMessageW(out MSG msg, IntPtr.Zero, 0, 0))
+            {
+                User32.TranslateMessage(ref msg);
+                User32.DispatchMessageW(ref msg);
+            }
+        }
+
+    }
+}
