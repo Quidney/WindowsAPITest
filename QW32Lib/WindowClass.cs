0539e7f766f38ab5d871d592cb680ad382b3c12e
Quidney <quidney0@gmail.com>
2024-10-21 23:33:25 +0200

    Remove WndProc from WindowClass

--- QW32Lib/WindowClass.cs
+++ QW32Lib/WindowClass.cs
@@ -1,5 +1,4 @@
 ﻿using QW32Lib.DataTypes.Delegates;
-using QW32Lib.DataTypes.Drawing;
 using QW32Lib.DataTypes.Helper;
 using QW32Lib.Enums;
 using System.Reflection;
@@ -10,7 +9,6 @@ namespace QW32Lib
     public class WindowClass
     {
         private const string DefaultClassName = "QW32WindowClass";
-
         public static WindowClass Default => defaultInstance.Value;
         private static readonly Lazy<WindowClass> defaultInstance = new(() => new WindowClass(className: DefaultClassName));
 
@@ -22,12 +20,10 @@ namespace QW32Lib
             return wndClass;
         }
         private static Dictionary<IntPtr, WindowClass> handleWndClassDictionary = [];
-
         public IntPtr HInstance { get; }
         public string ClassName { get; }
 
-
-        private WndProcDelegate dWndProc;
+        private WndProcDelegate WndProcDelegate;
         private IntPtr lpfnWndProc;
 
         private WNDCLASSEXW WNDCLASSEXW;
@@ -36,43 +32,15 @@ namespace QW32Lib
         {
             ClassName = className;
 
-            dWndProc = WndProc;
+            WndProcDelegate = Window.WndProc;
+            lpfnWndProc = Marshal.GetFunctionPointerForDelegate(WndProcDelegate);
 
-            lpfnWndProc = Marshal.GetFunctionPointerForDelegate(dWndProc);
             HInstance = Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]);
 
             WNDCLASSEXW = CreateWNDCLASSEXW();
             RegisterClass();
 
             handleWndClassDictionary.Add(HInstance, this);
-        }
-
-        protected virtual IntPtr WndProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam)
-        {
-            switch (uMsg)
-            {
-                case WindowNotification.WM_CREATE:
-                    break;
-
-                case WindowNotification.WM_DESTROY:
-                    if (Window.TryGetWindowFromHandle(hWnd, out Window? window))
-                    {
-                        window.Destroy();
-                    }
-                    break;
-
-                case WindowNotification.WM_PAINT:
-                    {
-                        IntPtr hdc = User32.BeginPaint(hWnd, out PAINTSTRUCT ps);
-
-                        User32.EndPaint(hwnd, ps);
-                    }
-
-                default:
-                    return User32.DefWindowProcW(hWnd, uMsg, wParam, lParam);
-            }
-
-            return IntPtr.Zero;
         }
 
         private unsafe WNDCLASSEXW CreateWNDCLASSEXW() => new()
