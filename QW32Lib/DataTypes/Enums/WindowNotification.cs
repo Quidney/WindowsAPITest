70c012d95357f08a51a4d0b1ee153a9345b4238c
Quidney <quidney0@gmail.com>
2024-10-21 23:32:23 +0200

    WindowNotification is not a class anymore but an enum of type uint

--- QW32Lib/DataTypes/Enums/WindowNotification.cs
+++ QW32Lib/DataTypes/Enums/WindowNotification.cs
@@ -1,40 +1,42 @@
 ﻿namespace QW32Lib.Enums
 {
-    internal static class WindowNotification
+    internal enum WindowNotification : uint
     {
-        public const uint WM_ACTIVATEAPP = 0x001C;              // 0x001C
-        public const uint WM_CANCELMODE = 0x001F;               // 0x001F
-        public const uint WM_CHILDACTIVATE = 0x0021;            // 0x0021
-        public const uint WM_CLOSE = 0x0010;                    // 0x0010
-        public const uint WM_COMPACTING = 0x0011;               // 0x0011
-        public const uint WM_CREATE = 0x0001;                   // 0x0001
-        public const uint WM_DESTROY = 0x0002;                  // 0x0002
-        public const uint WM_DPICHANGED = 0x02E0;               // 0x02E0
-        public const uint WM_ENABLE = 0x000A;                   // 0x000A
-        public const uint WM_ENTERSIZEMOVE = 0x0231;            // 0x0231
-        public const uint WM_EXITSIZEMOVE = 0x0232;             // 0x0232
-        public const uint WM_GETICON = 0x007F;                  // 0x007F
-        public const uint WM_GETMINMAXINFO = 0x0024;            // 0x0024
-        public const uint WM_INPUTLANGCHANGE = 0x0051;          // 0x0051
-        public const uint WM_INPUTLANGCHANGEREQUEST = 0x0050;   // 0x0050
-        public const uint WM_MOVE = 0x0003;                     // 0x0003
-        public const uint WM_MOVING = 0x0216;                   // 0x0216
-        public const uint WM_NCACTIVATE = 0x0086;               // 0x0086
-        public const uint WM_NCCALCSIZE = 0x0083;               // 0x0083
-        public const uint WM_NCCREATE = 0x0081;                 // 0x0081
-        public const uint WM_NCDESTROY = 0x0082;                // 0x0082
-        public const uint WM_NULL = 0x0000;                     // 0x0000
-        public const uint WM_QUERYDRAGICON = 0x0037;            // 0x0037
-        public const uint WM_QUERYOPEN = 0x0013;                // 0x0013
-        public const uint WM_QUIT = 0x0012;                     // 0x0012
-        public const uint WM_SHOWWINDOW = 0x0018;               // 0x0018
-        public const uint WM_SIZE = 0x0005;                     // 0x0005
-        public const uint WM_SIZING = 0x0214;                   // 0x0214
-        public const uint WM_STYLECHANGED = 0x007D;             // 0x007D
-        public const uint WM_STYLECHANGING = 0x007C;            // 0x007C
-        public const uint WM_THEMECHANGED = 0x031A;             // 0x031A
-        public const uint WM_USERCHANGED = 0x0040;              // 0x0040
-        public const uint WM_WINDOWPOSCHANGED = 0x0047;         // 0x0047
-        public const uint WM_WINDOWPOSCHANGING = 0x0046;        // 0x0046
+        WM_ACTIVATEAPP = 0x001C,              // 0x001C
+        WM_CANCELMODE = 0x001F,               // 0x001F
+        WM_CHILDACTIVATE = 0x0021,            // 0x0021
+        WM_CLOSE = 0x0010,                    // 0x0010
+        WM_COMPACTING = 0x0011,               // 0x0011
+        WM_CREATE = 0x0001,                   // 0x0001
+        WM_DESTROY = 0x0002,                  // 0x0002
+        WM_DPICHANGED = 0x02E0,               // 0x02E0
+        WM_ENABLE = 0x000A,                   // 0x000A
+        WM_ENTERSIZEMOVE = 0x0231,            // 0x0231
+        WM_EXITSIZEMOVE = 0x0232,             // 0x0232
+        WM_GETICON = 0x007F,                  // 0x007F
+        WM_GETMINMAXINFO = 0x0024,            // 0x0024
+        WM_INPUTLANGCHANGE = 0x0051,          // 0x0051
+        WM_INPUTLANGCHANGEREQUEST = 0x0050,   // 0x0050
+        WM_MOVE = 0x0003,                     // 0x0003
+        WM_MOVING = 0x0216,                   // 0x0216
+        WM_NCACTIVATE = 0x0086,               // 0x0086
+        WM_NCCALCSIZE = 0x0083,               // 0x0083
+        WM_NCCREATE = 0x0081,                 // 0x0081
+        WM_NCDESTROY = 0x0082,                // 0x0082
+        WM_NULL = 0x0000,                     // 0x0000
+        WM_QUERYDRAGICON = 0x0037,            // 0x0037
+        WM_QUERYOPEN = 0x0013,                // 0x0013
+        WM_QUIT = 0x0012,                     // 0x0012
+        WM_SHOWWINDOW = 0x0018,               // 0x0018
+        WM_SIZE = 0x0005,                     // 0x0005
+        WM_SIZING = 0x0214,                   // 0x0214
+        WM_STYLECHANGED = 0x007D,             // 0x007D
+        WM_STYLECHANGING = 0x007C,            // 0x007C
+        WM_THEMECHANGED = 0x031A,             // 0x031A
+        WM_USERCHANGED = 0x0040,              // 0x0040
+        WM_WINDOWPOSCHANGED = 0x0047,         // 0x0047
+        WM_WINDOWPOSCHANGING = 0x0046,        // 0x0046
+
+        WM_PAINT
     }
 }
